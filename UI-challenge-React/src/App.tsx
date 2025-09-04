6ae631f8043659a72947e3bb6fff51fc43676cb5
chaseSivuku <mxolisi.sivuku@gmail.com>
2025-09-04 09:20:44 +0200

    Created a small card with metrics

---
+++ UI-challenge-React/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import { SmallCard } from './components/SmallCard'
+
+function App() {
+
+  return (
+    <>
+      {/* <SmallCard
+        type="work"
+        time="32"
+        weekly='36'
+        header='icon-work.svg'
+        backgroundC='hsl(15, 100%, 70%)' 
+      ></SmallCard> */}
+    </>
+  )
+}
+
+export default App
---
+++ UI-challenge-React/src/components/SmallCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './SmallCard.module.css'
+
+type Props = {
+    type: string,
+    backgroundC?: string,
+    time: string,
+    weekly: string
+    header?: string
+}
+
+export const SmallCard: React.FC<Props> = ({type, backgroundC, time, weekly, header}) => {
+  return (
+    <>
+        <div className={styles.cardContainer}>
+            <div className={styles.header}
+                style={{
+                    backgroundColor: backgroundC,
+                    backgroundImage: `url(/images/${header})`
+                  
+                }}
+            >
+            </div>
+            <div className={styles.info}>
+                <div className={styles.heading}>
+                    <p>{type}</p>
+                    <button className={styles.topButton}><img src="/images/icon-ellipsis.svg" alt="..." /></button>
+                </div>
+                <div className={styles.metrics}>
+                    <p className={styles.time}>{time}hrs</p>
+                    <p className={styles.weekly}>Last week - {weekly}hrs</p>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
