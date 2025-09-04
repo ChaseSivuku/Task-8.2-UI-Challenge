56ce6555b3c1e0725b9ca9e8ea830101996743ec
chaseSivuku <mxolisi.sivuku@gmail.com>
2025-09-04 11:43:10 +0200

    created the profile card component and added everything to the page

--- UI-challenge-React/src/App.tsx
+++ UI-challenge-React/src/App.tsx
@@ -1,19 +1,65 @@
-import './App.css'
-// import { SmallCard } from './components/SmallCard'
+import "./App.css";
+import { SmallCard } from "./components/SmallCard";
+import { MainCard } from "./components/MainCard";
 
 function App() {
-
   return (
     <>
-      {/* <SmallCard
-        type="work"
-        time="32"
-        weekly='36'
-        header='icon-work.svg'
-        backgroundC='hsl(15, 100%, 70%)' 
-      ></SmallCard> */}
+      <div className="dashContainer">
+        <MainCard
+          name="Jeremy"
+          surname="Robson"
+          pic="image-jeremy.png"
+        ></MainCard>
+        <div className="metricsGrid">
+          <SmallCard
+            type="Work"
+            time="32"
+            weekly="36"
+            header="icon-work.svg"
+            backgroundC="hsl(15, 100%, 70%)"
+          ></SmallCard>
+          <SmallCard
+            type="Play"
+            time="10"
+            weekly="8"
+            header="icon-play.svg"
+            backgroundC="hsl(195, 74%, 62%)"
+          ></SmallCard>
+          <SmallCard
+            type="Study"
+            time="4"
+            weekly="7"
+            header="icon-study.svg"
+            backgroundC="hsl(348, 100%, 68%)"
+          ></SmallCard>
+          <SmallCard
+            type="Exercise"
+            time="4"
+            weekly="5"
+            header="icon-exercise.svg"
+            backgroundC="hsl(145, 58%, 55%)"
+          ></SmallCard>
+          <SmallCard
+            type="Social"
+            time="5"
+            weekly="10"
+            header="icon-social.svg"
+            backgroundC="hsl(264, 64%, 52%)"
+          ></SmallCard>
+          <SmallCard
+            type="Self Care"
+            time="2"
+            weekly="2"
+            header="icon-self-care.svg"
+            backgroundC="hsl(43, 84%, 65%)"
+          ></SmallCard>
+          
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ UI-challenge-React/src/components/MainCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './MainCard.module.css'
+import stylesTwo from './SmallCard.module.css'
+
+type Props = {
+    name?: string,
+    surname?: string, 
+    pic?: string
+}
+
+export const MainCard: React.FC <Props> = ({name, surname, pic}) => {
+  return (
+    <>
+        <div className={styles.cardContainer}>
+            <div className={styles.profile}>
+                <div className={styles.profilePic}>
+                    <img src={`/images/${pic}`} alt="" />
+                </div>
+                <div className={styles.profileNames}>
+                    <p className={stylesTwo.weekly}>Report for</p>
+                    <p className={styles.names}>{name}</p>
+                    <p className={styles.names}>{surname}</p>
+                </div>
+            </div>
+            <div className={styles.intervals}>
+                <div className={`${styles.values} ${stylesTwo.weekly}`}>
+                    <ul>
+                        <li>Daily</li>
+                        <li>Weekly</li>
+                        <li>Monthly</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
